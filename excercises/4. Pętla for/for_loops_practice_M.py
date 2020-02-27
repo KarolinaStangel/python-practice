fc57de06c1cd8dda7bb140a36992677b4afedc75
Wodianoj <kuei-jin@o2.pl>
2020-02-27 19:01:44 +0100

    02 i 03 - drugie i trzecie też...

--- excercises/4. Pętla for/for_loops_practice_M.py
+++ excercises/4. Pętla for/for_loops_practice_M.py
@@ -2,7 +2,18 @@ fruits = ["apple", "pear", "orange", "banana"]
 
 print("Fruits: ")
 for fruit in fruits:
-    print("- ", fruit+",")
+    print("- ", fruit + ",")
+print("_______________")
 
+print("Fruits: ")
+size = len(fruits)
+for i in range(size):
+    print(str(i + 1) + ".", fruits[i])
+print("_______________")
 
-
+print("Fruits: ")
+size = len(fruits)
+i = 0
+while i < size:
+    print(str(i + 1) + ".", fruits[i])
+    i += 1
