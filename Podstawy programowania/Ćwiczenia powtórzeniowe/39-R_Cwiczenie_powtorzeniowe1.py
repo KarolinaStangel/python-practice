af2f0dd7b8f31a076d5aabc46a12f0aa78bed0e2
Wodianoj <kuei-jin@o2.pl>
2020-03-22 22:11:04 +0100

    Cwiczenia z platformy

--- Podstawy programowania/Ćwiczenia powtórzeniowe/39-R_Cwiczenie_powtorzeniowe1.py
+++ Podstawy programowania/Ćwiczenia powtórzeniowe/39-R_Cwiczenie_powtorzeniowe1.py
@@ -2,14 +2,14 @@
 # Rozwiązać równanie ax+b=0.
 
 def get_number(name):
-    num = input("Podaj liczbę " + name + ": ")
+    number = input("Podaj liczbę " + name + ": ")
     while True:
       try:
-          num = float(num)
+          number = float(number)
           break
       except:
-          num = input("Błąd, podaj liczbę: ")
-    return num
+          number = input("Błąd, podaj liczbę: ")
+    return number
 
 def get_decision():
     choice = input("Czy chcesz policzyć jeszcze raz? (t/n) ")
@@ -21,7 +21,7 @@ def get_decision():
             return choice
         choice = input("Błędny wybór! Wybierz 't' lub 'n': ")
 
-def prevent_0_div():
+def prevent_0_division():
     a = get_number("a")
     while True:
         if a != 0:
@@ -33,7 +33,7 @@ def prevent_0_div():
 choice = "t"
 while choice == "t":
     print("Mamy równanie: ax+b=0. \nW celu obliczenia warości x proszę wprowadzić wartości dla zmiennych a i b:")
-    a = prevent_0_div()
+    a = prevent_0_division()
     b = get_number("b")
     x = (-b)/a
     print("x =", round(x, 2))
