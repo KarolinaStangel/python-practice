cc034827e7dce6517e7a89476d61ff8c7caf2c5e
Wodianoj <kuei-jin@o2.pl>
2020-03-22 22:06:01 +0100

    Cwiczenia z platformy

--- Podstawy programowania/Ćwiczenia powtórzeniowe/39-R_Cwiczenie_powtorzeniowe1.py
+++ Podstawy programowania/Ćwiczenia powtórzeniowe/39-R_Cwiczenie_powtorzeniowe1.py
@@ -1,8 +1,8 @@
 # Zadanie 1
 # Rozwiązać równanie ax+b=0.
 
-def num_choice(name):
-    num = input("Podaj liczbę "+name+": ")
+def get_number(name):
+    num = input("Podaj liczbę " + name + ": ")
     while True:
       try:
           num = float(num)
@@ -11,7 +11,7 @@ def num_choice(name):
           num = input("Błąd, podaj liczbę: ")
     return num
 
-def decision():
+def get_decision():
     choice = input("Czy chcesz policzyć jeszcze raz? (t/n) ")
     while True:
         if choice == "t":
@@ -21,21 +21,21 @@ def decision():
             return choice
         choice = input("Błędny wybór! Wybierz 't' lub 'n': ")
 
-def no_0_division():
-    a = num_choice("a")
+def prevent_0_div():
+    a = get_number("a")
     while True:
         if a != 0:
             return a
         else:
             print("Nie dzieli się przez 0.")
-            a = num_choice("a")
+            a = get_number("a")
 
 choice = "t"
 while choice == "t":
     print("Mamy równanie: ax+b=0. \nW celu obliczenia warości x proszę wprowadzić wartości dla zmiennych a i b:")
-    a = no_0_division()
-    b = num_choice("b")
+    a = prevent_0_div()
+    b = get_number("b")
     x = (-b)/a
     print("x =", round(x, 2))
-    choice = decision()
+    choice = get_decision()
 
