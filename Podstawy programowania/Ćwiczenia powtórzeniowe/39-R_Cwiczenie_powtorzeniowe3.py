584ece2ef7ce95c78cbdbc1ee14935db9a1cb92c
Wodianoj <kuei-jin@o2.pl>
2020-03-23 18:16:54 +0100

    Cwiczenia z platformy3

--- Podstawy programowania/Ćwiczenia powtórzeniowe/39-R_Cwiczenie_powtorzeniowe3.py
+++ Podstawy programowania/Ćwiczenia powtórzeniowe/39-R_Cwiczenie_powtorzeniowe3.py
@@ -5,12 +5,15 @@
 # gdzie p jest połową obwodu.
 import math
 
-def get_number(name):
-    number = input("Podaj liczbę " + name + ": ")
+def get_side(name):
+    number = input("Podaj długość boku " + name + ": ")
     while True:
       try:
           number = float(number)
-          break
+          if number <= 0:
+              number = input("Długość boku trójkąta nie może być mniejsza lub równa 0. \nPodaj jeszcze raz długość boku " + name + " ")
+          else:
+            break
       except:
           number = input("Błąd, podaj liczbę: ")
     return number
@@ -27,7 +30,25 @@ def get_decision():
 
 choice = "t"
 while choice == "t":
+    print("Proszę podać długości boków trójkąta w celu obliczenia jego pola: ")
+    a = get_side("a")
+    b = get_side("b")
+    c = get_side("c")
+    while True:
+        if a >= b + c:
+            print("Bok a jest za długi aby mógł powstać trójkąt")
+            a = get_side("a")
+        elif b >= a + c:
+            print("Bok b jest za długi aby mógł powstać trójkąt")
+            b = get_side("b")
+        elif c >= a + b:
+            print("Bok c jest za długi aby mógł powstać trójkąt")
+            c = get_side("c")
+        else:
+            break
 
-
+    p = (a + b + c) / 2
+    triangle_area = math.sqrt(p*(p-a)*(p-b)*(p-c))
+    print("Pole trójkąta wynosi: " + str(round(triangle_area, 2)))
 
     choice = get_decision()
