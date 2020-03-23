14482c05eab19fec9602d2fe89db10ca0c4bd94d
Wodianoj <kuei-jin@o2.pl>
2020-03-23 12:49:11 +0100

    Cwiczenia z platformy3

---
+++ Podstawy programowania/Ćwiczenia powtórzeniowe/39-R_Cwiczenie_powtorzeniowe3.py
@@ -0,0 +1,26 @@
+# Zadanie 3
+# Wczytać długości 3 boków trójkąta. Sprawdzić czy z podanych trzech długości odcinków
+# można zbudować trójkąt, jeżeli tak obliczyć jego pole.
+# P=sqrt(p*(p-a)*(p-b)*(p-c)),
+# gdzie p jest połową obwodu.
+
+def get_number(name):
+    number = input("Podaj liczbę " + name + ": ")
+    while True:
+      try:
+          number = float(number)
+          break
+      except:
+          number = input("Błąd, podaj liczbę: ")
+    return number
+
+def get_decision():
+    choice = input("Czy chcesz policzyć jeszcze raz? (t/n) ")
+    while True:
+        if choice == "t":
+            return choice
+        elif choice == "n":
+            print("\nDziękuję za skorzystanie z mojego programu.\n")
+            return choice
+        choice = input("Błędny wybór! Wybierz 't' lub 'n': ")
+
